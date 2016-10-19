cbc21052631e766144d81c921474b94e78a4ab2f
jftoh <tohjianfeng@gmail.com>
2016-10-20 01:56:30 +0800

    added basic CLI; added batch report retrieval method to virustotal script

--- virustotal.py
+++ virustotal.py
@@ -12,108 +12,119 @@ import requests
 import json
 
 class Virustotal():
-	""" Virustotal API module """
-	def __init__(self):
-		self.host = "www.virustotal.com"
-		self.base = "https://www.virustotal.com/vtapi/v2/"
-		self.apikey = "0cf94a0fbb09cbbd69e5e264eb24e9e1d8607157acaa25d1ad58a407022456a0"
+    """ Virustotal API module """
+    def __init__(self):
+        self.host = "www.virustotal.com"
+        self.base = "https://www.virustotal.com/vtapi/v2/"
+        self.apikey = "0cf94a0fbb09cbbd69e5e264eb24e9e1d8607157acaa25d1ad58a407022456a0"
 
-	def rscReport(self, rsc):
-		""" Get latest report of resource """
+    def rscReport(self, rsc):
+        """ Get latest report of resource """
 
-		base = self.base + 'file/report'
-		parameters = {"resource":rsc, "apikey":self.apikey}
-		r = requests.post(base, data=parameters)
-		resp = r.json()
-		results = parse_resp(resp)
-		return results
+        base = self.base + 'file/report'
+        parameters = {"resource":rsc, "apikey":self.apikey}
+        r = requests.post(base, data=parameters)
+        resp = r.json()
+        results = parse_resp(resp)
+        return results
 
-	def urlReport(self, rsc, scan=0):
-		""" Get latest report URL scan report of resource """
+    def rscBatchReport(self, rsc):
+        """ Get latest report of a batch of resource """
 
-		base = self.base + 'url/report'
-		parameters = {"resource":rsc, "scan":scan, "apikey":self.apikey}
-		r = requests.post(base, data=parameters)
-		resp = r.json()
-		results = parse_resp(resp)
-		return results
+        base = self.base + 'file/report'
+        parameters = {"resource":rsc, "apikey":self.apikey}
+        r = requests.post(base, data=parameters)
+        resp = r.json()
+        results = parse_resp(resp)
+        return results
+   
 
-	def ipReport(self, rsc):
-		""" Get latest report for IP Address """
+    def urlReport(self, rsc, scan=0):
+        """ Get latest report URL scan report of resource """
 
-		base = self.base + 'ip-address/report'
-		parameters = {"ip":rsc, "apikey":self.apikey}
-		r = requests.get(base, params=parameters)
-		resp = r.json()
-		results = parse_resp(resp)
-		return results
+        base = self.base + 'url/report'
+        parameters = {"resource":rsc, "scan":scan, "apikey":self.apikey}
+        r = requests.post(base, data=parameters)
+        resp = r.json()
+        results = parse_resp(resp)
+        return results
 
-	def domainReport(self, rsc):
-		""" Get latest report for IP Address """
+    def ipReport(self, rsc):
+        """ Get latest report for IP Address """
 
-		base = self.base + 'domain/report'
-		parameters = {"domain":rsc, "apikey":self.apikey}
-		r = requests.get(base, params=parameters)
-		resp = r.json()
-		results = parse_resp(resp)
-		return results
-		
-	def scanURL(self, rsc):
+        base = self.base + 'ip-address/report'
+        parameters = {"ip":rsc, "apikey":self.apikey}
+        r = requests.get(base, params=parameters)
+        resp = r.json()
+        results = parse_resp(resp)
+        return results
 
-		""" Send RSC/URL for scanning; Its encouraged to check for last scanusing urlReport()
-		To submit batch rsc should be example.com\nexample2.com"""
+    def domainReport(self, rsc):
+        """ Get latest report for IP Address """
 
-		base = self.base + 'url/scan'
-		parameters = {"url":rsc, "apikey":self.apikey}
-		r = requests.post(base, data=parameters)
-		resp = r.json()
-		results = parse_resp(resp)
-		return results
+        base = self.base + 'domain/report'
+        parameters = {"domain":rsc, "apikey":self.apikey}
+        r = requests.get(base, params=parameters)
+        resp = r.json()
+        results = parse_resp(resp)
+        return results
+        
+    def scanURL(self, rsc):
 
-	def rscSubmit(self, rsc):
+        """ Send RSC/URL for scanning; Its encouraged to check for last scanusing urlReport()
+        To submit batch rsc should be example.com\nexample2.com"""
 
-		""" Submit potential malicious file to virustotal for analyzing """
-		base = self.base + 'file/scan'
-		f = open(rsc, 'rb')
-		parameters = {"apikey":self.apikey}
-		r = requests.post(base, data=parameters, files={'file':f})
-		resp = r.json()
-		results = parse_resp(resp)
-		return results
+        base = self.base + 'url/scan'
+        parameters = {"url":rsc, "apikey":self.apikey}
+        r = requests.post(base, data=parameters)
+        resp = r.json()
+        results = parse_resp(resp)
+        return results
 
-	def rscRescan(self, rsc):
+    def rscSubmit(self, rsc):
 
-		""" Rescan potential malicious file to virustotal for analyzing without uploading the file again """
-		base = self.base + 'file/rescan'
-		parameters = {"resource":rsc, "apikey":self.apikey}
-		r = requests.post(base, data=parameters)
-		resp = r.json()
-		results = parse_resp(resp)
-		return results
+        """ Submit potential malicious file to virustotal for analyzing """
+        base = self.base + 'file/scan'
+        f = open(rsc, 'rb')
+        parameters = {"apikey":self.apikey}
+        r = requests.post(base, data=parameters, files={'file':f})
+        resp = r.json()
+        results = parse_resp(resp)
+        return results
 
-	def postComment(self, rsc, comment):
+    def rscRescan(self, rsc):
 
-		""" Post comment to files or urls """
-		base = self.base + 'comments/put'
-		parameters = {"resource":rsc, "comment":comment, "apikey":self.apikey}
-		r = requests.post(base, data=parameters)
-		resp = r.json()
-		results = parse_resp(resp)
-		if results['response_code'] == 0:
-			print "Oh no something happen...cant post comment"
-		else:
-			print "Your comment was successfully posted"
-			call = self.rscReport(rsc)
-			for item in call:
-				if item == "permalink":
-					print "Report link:", call[item]
+        """ Rescan potential malicious file to virustotal for analyzing without uploading the file again """
+        base = self.base + 'file/rescan'
+        parameters = {"resource":rsc, "apikey":self.apikey}
+        r = requests.post(base, data=parameters)
+        resp = r.json()
+        results = parse_resp(resp)
+        return results
+
+    def postComment(self, rsc, comment):
+
+        """ Post comment to files or urls """
+        base = self.base + 'comments/put'
+        parameters = {"resource":rsc, "comment":comment, "apikey":self.apikey}
+        r = requests.post(base, data=parameters)
+        resp = r.json()
+        results = parse_resp(resp)
+        if results['response_code'] == 0:
+            print "Oh no something happen...cant post comment"
+        else:
+            print "Your comment was successfully posted"
+            call = self.rscReport(rsc)
+            for item in call:
+                if item == "permalink":
+                    print "Report link:", call[item]
 
 
 def parse_resp(resp):
-	""" Parses the response from the requests.gets/posts()
-	then returns the data back to the function """
-	buf = {}
-	for item in resp:
-		buf[item] = resp[item]
+    """ Parses the response from the requests.gets/posts()
+    then returns the data back to the function """
+    buf = {}
+    for item in resp:
+        buf[item] = resp[item]
 
-	return buf
+    return buf
---
+++ vt-cli.py
@@ -0,0 +1,84 @@
+import argparse
+import sys
+
+from virustotal import *
+from database import db
+
+class VirusTotalCli:
+    """ Command-line interface to retrieve scan results """
+
+    MACdb = db()
+    vt_api = VirusTotal()
+
+    def __init__(self):
+        arg_parser = argparse.ArgumentParser(description='Command-line Interface to retrieve VirusTotal Scan results', usage='python vt-cli.py <command> [<args>]')
+
+        arg_parser.add_argument('command', help='Subcommand to run')
+
+        # parse_args defaults to [1:] for args, but you need to
+        # exclude the rest of the args too, or validation will fail
+        args = arg_parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            print 'Unrecognized command'
+            arg_parser.print_help()
+            exit(1)
+
+        # use dispatch pattern to invoke method with same name
+        getattr(self, args.command)()
+
+    def search(self):
+        """ Searches the database for a file and makes a request to the VirusTotal server for its corresponding result. """
+
+        arg_parser = argparse.ArgumentParser(description="Searches the database for a specific file and retrieves the scan results from VirusTotal.")
+
+        arg_parser.add_argument("file_name", help="File Name")
+        arg_parser.add_argument("-v", "--verbose", action="store_true", help="Display scan results with more detail")
+
+        # we ignore the first (main) command and parse the subcommands and its flags.
+        args = arg_parser.parse_args(sys.argv[2:])
+
+        #TODO(@jftoh) finish method to search for a specific file
+    
+    def list(self):
+        """ Retrieves the scan results up to the last 10 files. """
+
+        arg_parser = argparse.ArgumentParser(description="Retrieves the scan results up to the last 10 files. ")
+
+        # retrieve all results from database
+        file_list = MACdb.getAllScanResults()
+
+        # exit program if database is empty
+        if len(file_list) == 0:
+             print("Database is empty.")
+             exit(1)
+
+        max_list_size = len(file_list)
+        if len(file_list) > 10:
+            max_list_size = 10
+
+        # create an empty list to start appending the resource hashes
+        resource_list = []
+
+        # append the first file scan ID to the list
+        resource_list.append(file_list[0].getFileName())
+
+        if max_list_size > 1:
+            # append the remaining file scan IDs to the list
+            for file_details in file_list[1:max_list_size]:
+                resource_list.append(', ' + file_details.getFileName())
+
+        res_str = ''.join(resource_list)
+
+        # Retrieve scan results using VirusTotal API
+        batch_scan_results = vt_api.resBatchReport(res_str)
+
+        for scan_result in batch_scan_results:
+            if scan_result['response_code'] == 1:
+                print("Results for resource ID: {}\n".format(scan_result['resource']))
+                print("Scan Date: {}\n".format(scan_result['scan_date']))
+                print("Number of positives: {}/{}\n".format(scan_result['positives'], scan_result['total']))
+
+
+if __name__ == '__main__':
+  VirusTotalCli()
+
